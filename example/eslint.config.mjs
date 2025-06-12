9fda402d3af706f42c6c7f73a4f1084baf8faac0
okonomi <okonomi@oknm.jp>
2025-06-13 00:56:46 +0900

    feat: add practice plugin

--- example/eslint.config.mjs
+++ example/eslint.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "eslint/config"
 import globals from "globals"
 import js from "@eslint/js"
+import practice from "@okonomi/eslint-plugin-practice"
 
 export default defineConfig([
   {
-    files: ["**/*.js"],
+    files: ["**/*.{js,mjs}"],
     extends: [
       js.configs.recommended,
     ],
+    plugins: {
+      practice
+    },
     languageOptions: {
       globals: {
         ...globals.node,
