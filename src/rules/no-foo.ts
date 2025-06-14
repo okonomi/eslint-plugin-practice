4b3d2eaded7293e7284178e481a258dd7270d044
okonomi <okonomi@oknm.jp>
2025-06-14 16:15:28 +0900

    implement rule with ESLintUtils

--- src/rules/no-foo.ts
+++ src/rules/no-foo.ts
@@ -1,11 +1,29 @@
-export default {
+import { ESLintUtils } from '@typescript-eslint/utils'
+
+const createRule = ESLintUtils.RuleCreator(
+  name => `https://example.com/rule/${name}`,
+);
+
+export default createRule({
+  name: 'no-foo',
   create(context) {
     return {
       Identifier(node) {
         if (node.name === "foo") {
-          context.report({ node, message: "'foo' is not allowed." });
+          context.report({ node, messageId: 'noFoo' });
         }
       }
     }
-  }
-}
+  },
+  meta: {
+    docs: {
+      description: 'Disallow the use of "foo" as an identifier.',
+    },
+    messages: {
+      noFoo: '"foo" is not allowed.',
+    },
+    type: 'suggestion',
+    schema: [],
+  },
+  defaultOptions: [],
+})
